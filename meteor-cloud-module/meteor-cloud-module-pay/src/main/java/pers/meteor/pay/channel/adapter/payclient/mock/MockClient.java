2ef95983363cba03753da253d3e0c52fa366131e
dream-colors <56667376+dream-colors@users.noreply.github.com>
2024-07-09 00:21:38 +0800

    通道域代码

--- meteor-cloud-module/meteor-cloud-module-pay/src/main/java/pers/meteor/pay/channel/adapter/payclient/mock/MockClient.java
+++ meteor-cloud-module/meteor-cloud-module-pay/src/main/java/pers/meteor/pay/channel/adapter/payclient/mock/MockClient.java
@@ -1,6 +1,5 @@
 package pers.meteor.pay.channel.adapter.payclient.mock;
 
-import pers.meteor.pay.adapter.channel.payclient.mock.MockClientConfig;
 import pers.meteor.pay.channel.domain.service.impl.AbstractPayClient;
 
 /**

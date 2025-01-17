c087b8f979028f083a82fb0fa6e7569adc4eb829
dream-colors <56667376+dream-colors@users.noreply.github.com>
2025-01-17 23:31:15 +0800

    整合基础模块

---
+++ meteor-modules/meteor-module-shared/meteor-shared/src/main/java/pers/meteor/shared/SharedApplication.java
@@ -0,0 +1,18 @@
+package pers.meteor.shared;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 系统模块
+ *
+ * @author meteor
+ */
+@SpringBootApplication
+public class SharedApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SharedApplication.class, args);
+    }
+}
